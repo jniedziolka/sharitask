3e98a655e53ac7cd3a4c90bd27b9ea41b8da314a
jniedziolka <jakub.niedziolka@o2.pl>
2021-03-12 10:22:38 +0100

    Added user as the shared (lazy) data

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        Here comes the dashboard.
-        <inertia-link type="button" href="/logout" method="post">Logout</inertia-link>
+        Here comes the dashboard of {{ user.nickname }}.
+        <inertia-link type="button" href="/logout" method="post" class="p-4 bg-gray-200">Logout</inertia-link>
     </div>
 </template>
 
 <script>
     export default {
-
+        computed: {
+            user() {
+                return this.$page.props.user;
+            }
+        },
     }
 </script>
